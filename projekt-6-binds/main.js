a9ec1da6ad5a4d73089dcdd6cb909dba9e71926b
Marcin Galera <marcin.galera@outlook.com>
2019-04-06 20:56:06 +0200

    prototypes

---
+++ projekt-6-binds/main.js
@@ -0,0 +1,71 @@
+// function talk() {
+//     return this.sound;
+// }
+
+// var bark = function(target, target2) {
+//     return `${this.sound} do a dog ${target}, ${target2}`
+// }
+
+// let dog = {
+//     sound: ' hau hau',
+// }
+
+// let cat = {
+//     sound: 'miau miau'
+// }
+
+// let boundedDog = talk.bind(cat);
+// console.log(boundedDog())
+
+// let calledDog = bark.call(dog, 'sss', 'ddd')
+// console.log(calledDog)
+
+// let applyDog = bark.apply(dog, ['ggg', 'ddd'])
+// console.log(applyDog)
+
+
+
+
+
+
+
+
+// var animals = {
+//     eat: function() {
+//         return 'Dont eat ' + this.name;
+//     }
+// };
+
+// var cat = Object.create(animals, {
+//     'name': {
+//         value: 'milk',
+//         enumerable:false,
+//         writable:false,
+//     }
+// })
+
+// console.log(cat.eat(), cat)
+
+
+
+
+
+
+
+
+// let a = {name: 'aaa'};
+// let b = {lastName: 'bbb'};
+
+// let ab = Object.assign({}, a, b)
+// console.log(ab)
+
+
+var favouriteDrink = 'whisky';
+
+function changeFavourite() {
+    var favouriteDrink = 'wine'
+    return favouriteDrink;
+}
+
+changeFavourite();
+console.log(favouriteDrink);
